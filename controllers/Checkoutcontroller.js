11de7df95becfe8119ede0efd8a75a634aac3e83
Muhammed Gasal <gasalgasal246@gmail.com>
2023-11-17 06:21:41 +0530

    Solved address bug on checkout now you can checkout with any new adress from check also

--- controllers/Checkoutcontroller.js
+++ controllers/Checkoutcontroller.js
@@ -14,27 +14,29 @@ module.exports = {
         const product = await Products.findById(pid);
         const user = await usermodel.findById(req.cookies.user.id);
         const address = await Address.findOne({ Userid: user._id });
-        const wallet = await Wallets.findOne({ Userid: req.cookies.user.id });
-        let total = (product.Price - product.Discount) * qty
-        let Coupon
-        if(coupon){
-          Coupon = await Coupons.findOne({ Code: coupon })
-          const disc = (Coupon.Discount / 100) * total
-          total = total - disc
-        }
-        res.render("user/checkout", {
-          product: product,
-          user: user,
-          address: address,
-          quantity: qty ? parseInt(qty) : 1,
-          cart: null,
-          wallet, total, coupon: Coupon
-        });
-      } 
+          const wallet = await Wallets.findOne({ Userid: req.cookies.user.id });
+          let total = (product.Price - product.Discount) * qty;
+          let Coupon;
+          if (coupon) {
+            Coupon = await Coupons.findOne({ Code: coupon });
+            const disc = (Coupon.Discount / 100) * total;
+            total = total - disc;
+          }
+          res.render("user/checkout", {
+            product: product,
+            user: user,
+            address: address?address: null,
+            quantity: qty ? parseInt(qty) : 1,
+            cart: null,
+            wallet,
+            total,
+            coupon: Coupon
+          });
+      }
     } catch (error) {
       const on = "On Checkout Order";
       const err = error.message;
       res.redirect("/error?err=" + err + "&on=" + on);
     }
-  },
+  }
 };
